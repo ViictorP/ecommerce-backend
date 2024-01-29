c2970f19d814c352e553c7017f55625723f28a04
Victor Pimentel Correia <victorpimentelw@gmail.com>
2024-01-29 11:54:00 -0300

    Configuração de segurança para endpoints

--- src/main/java/com/victor/backend/ecommerce/ecommercebackend/api/security/WebSecurityConfig.java
+++ src/main/java/com/victor/backend/ecommerce/ecommercebackend/api/security/WebSecurityConfig.java
@@ -1,18 +1,25 @@
 package com.victor.backend.ecommerce.ecommercebackend.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
 
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //TODO: Proper authentication.
         http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/produto", "/auth/registrar", "/auth/login").permitAll()
+                .anyRequest().authenticated());
         return http.build();
     }
 }
